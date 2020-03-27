9cdf6db07dc78c68d95fc7230ebf6866e3f95282
Fred Strauss <fred@strider.co.za>
2020-03-27 21:44:13 +0200

    Basic Task test

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import Task
 
 main :: IO ()
 main = do
-  putStrLn "Hello Task Wizard!"
+  putStrLn $ show $ taskFromString 1 "Test task"
--- src/Task.hs
+++ src/Task.hs
@@ -3,15 +3,23 @@ module Task
   , ID
   , Task (..)
   , Action
+  , taskFromString
   ) where
 
 type Desc = String
 type ID   = Integer
 
 data Task = Task
-  { id :: ID
+  { uuid :: ID
   , desc :: Desc
-  }
+  } deriving (Eq, Show)
 
 data Action = Add Desc
             | Done ID
+
+taskFromString :: ID -> String -> Maybe Task
+taskFromString _ [] = Nothing
+taskFromString x s = Just Task
+  { uuid = x
+  , desc = s
+  }
