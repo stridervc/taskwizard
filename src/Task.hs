b38dc01f7cff39a7db8fe684e7d960c80fa250b8
Fred Strauss <fred@strider.co.za>
2020-04-05 13:05:09 +0200

    Implement parseAddTask

--- src/Task.hs
+++ src/Task.hs
@@ -8,7 +8,7 @@ module Task
   , Task (..)
   , Action (..)
   , uidExists
-  , nextUuid
+  , nextUid
   , addExactTask
   , deleteTask
   , printTask
@@ -16,13 +16,16 @@ module Task
   , applyAction
   , applyActions
   , stringToExact
+  , parseAddTask
   ) where
 
 import GHC.Generics (Generic)
 import Data.Binary
 
-type Desc = String
-type ID   = Integer
+type Desc   = String
+type ID     = Integer
+type Key    = String
+type Value  = String
 
 type Tasks = [Task]
 type Actions = [Action]
@@ -41,11 +44,60 @@ data Action = Exact Task
 instance Binary Task
 instance Binary Action
 
--- custom show for task, used for saving and loading
+-- custom show for task, used for saving
 instance Show Task where
-  show t = "uid:"++i++" isdone:"++isd ++ " " ++ desc t
+  show t = "uid:"++i++" done:"++isd ++ " " ++ desc t
     where i   = show $ uid t
           isd = show $ isdone t
+
+-- create a new, empty task
+newTask :: ID -> Task
+newTask i = Task  { uid = i
+                  , isdone = False
+                  , desc = ""
+                  }
+
+-- parse and add a task to tasks
+parseAddTask :: Tasks -> String -> Tasks
+parseAddTask ts s = t:ts
+  where t   = foldl applyIfIsProperty t' w
+        t'  = t'' {desc = d}
+        t'' = newTask $ nextUid ts
+        w   = words s
+        d   = unwords $ filter (not . isProperty) w
+
+splitProperty :: String -> Maybe (Key, Value)
+splitProperty s
+  | ':' `elem` s  = Just (k,v)
+  | otherwise     = Nothing
+  where k = takeWhile (/= ':') s
+        v = tail $ dropWhile (/= ':') s
+
+-- check if a string is a known property
+isProperty :: String -> Bool
+isProperty s = do
+  let p = splitProperty s
+  case p of
+    Nothing -> False
+    Just (k,v) -> do
+      case k of
+        "uid"     -> True
+        "done"    -> True
+        otherwise -> False
+
+-- apply iff property
+-- take a task and a string
+-- if the string is a known property (eg uid:1)
+-- then apply the property to the task
+applyIfIsProperty :: Task -> String -> Task
+applyIfIsProperty t s
+  | isProperty s  = do
+    let Just (k,v) = splitProperty s
+    case k of
+      "uid"     -> t {uid = read v}
+      "done"    -> t {isdone = read v} 
+      otherwise -> t
+  | otherwise     = t
 
 -- apply an action to a list of tasks
 applyAction :: Tasks -> Action -> Tasks
@@ -59,9 +111,9 @@ applyActions = foldl applyAction
 uidExists :: Tasks -> ID -> Bool
 uidExists ts i = foldr (\t acc -> if uid t == i then True else acc) False ts
 
-nextUuid :: Tasks -> ID
-nextUuid [] = 1
-nextUuid ts = head $ dropWhile (uidExists ts) [1..]
+nextUid :: Tasks -> ID
+nextUid [] = 1
+nextUid ts = head $ dropWhile (uidExists ts) [1..]
 
 -- helper function, add task with all fields provided
 -- will be used when loading tasks from file
@@ -71,7 +123,7 @@ addExactTask ts t = t:ts
 -- convert from desc string to an Exact action
 stringToExact :: Tasks -> String -> Action
 stringToExact ts d =
-  Exact Task  { uid     = nextUuid ts
+  Exact Task  { uid     = nextUid ts
               , desc    = d
               , isdone  = False
               }
